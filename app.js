924c8a8f53dfc41e2f127cd697d75ef262566d69
Alex <thatsoalex@gmail.com>
2022-04-14 09:07:12 -0500

    Uploaded all problems

---
+++ app.js
@@ -0,0 +1,272 @@
+
+//Dataset
+let dishes = [
+    {
+        "id": 1,
+        "name": "Pizza",
+        "cuisine": "Italian",
+        "servings": 8,
+        "ingredients": ["tomato", "cheese"]
+    },
+    {
+        "id": 2,
+        "name": "Spaghetti",
+        "cuisine": "Italian",
+        "servings": 2,
+        "ingredients": ["tomato", "cheese"]
+    },
+    {
+        "id": 3,
+        "name": "Ravioli",
+        "cuisine": "Italian",
+        "servings": 2,
+        "ingredients": ["tomato", "cheese"]
+    },
+    {
+        "id": 4,
+        "name": "Enchiladas",
+        "cuisine": "Mexican",
+        "servings": 6,
+        "ingredients": ["tomato", "cheese"]
+    },
+    {
+        "id": 5,
+        "name": "Tacos",
+        "cuisine": "Mexican",
+        "servings": 4,
+        "ingredients": ["tomato", "cheese"]
+    },
+    {
+        "id": 6,
+        "name": "Burrito Supreme",
+        "cuisine": "Mexican",
+        "servings": 1,
+        "ingredients": ["tomato", "cheese"]
+    },
+    {
+        "id": 7,
+        "name": "Elote",
+        "cuisine": "Mexican",
+        "servings": 4,
+        "ingredients": ["corn", "cheese"]
+    },
+    {
+        "id": 8,
+        "name": "Crepes",
+        "cuisine": "French",
+        "servings": 1,
+        "ingredients": ["flour", "sugar"]
+    },
+    {
+        "id": 9,
+        "name": "Corned Beef & Cabbage",
+        "cuisine": "Irish",
+        "servings": 10,
+        "ingredients": ["beef", "cabbage"]
+    },
+    {
+        "id": 10,
+        "name": "Beef Stew",
+        "cuisine": "Irish",
+        "servings": 8,
+        "ingredients": ["tomato", "beef"]
+    },
+    {
+        "id": 11,
+        "name": "Lasagna",
+        "cuisine": "Vegetarian",
+        "servings": 8,
+        "ingredients": ["tomato", "cheese"]
+    },
+    {
+        "id": 12,
+        "name": "Falafel",
+        "cuisine": "Vegetarian",
+        "servings": 1,
+        "ingredients": ["chickpea", "parsley"]
+    },
+    {
+        "id": 13,
+        "name": "Chili",
+        "cuisine": "Vegetarian",
+        "servings": 13,
+        "ingredients": ["tomato", "chickpea"]
+    },
+    {
+        "id": 14,
+        "name": "Goulash",
+        "cuisine": "Hungarian",
+        "servings": 15,
+        "ingredients": ["tomato", "beef"]
+    },
+
+    
+]
+
+//Example function
+function findMexicanFood(){
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Mexican"){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+    //Debug tip: Place a Breakpoint on the return AFTER the filter, then hover over "results" to see results of filter without having to step through the whole thing!
+
+    return results;
+}
+
+let mexicanFood = findMexicanFood();
+console.log('Mexican Foods: ', mexicanFood)
+
+
+
+//1. Create a function that will return all dishes with the cuisine type of "vegetarian"
+//Filter
+
+function problemOne(){
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Vegetarian"){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+    return results;
+}
+
+let VegetarianFood = problemOne();
+console.log('Vegetarian Foods: ', VegetarianFood)
+
+//2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
+//Filter
+function problemTwo(){
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Italian" && el.servings > 5){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+    return results;
+}
+
+let ItalianFood = problemTwo();
+console.log('Italian Foods: ', ItalianFood)
+
+
+
+//3. Create a function that will return only dishes whose serving id number matches their serving count.
+//Filter
+function problemThree(){
+    let results = dishes.filter(function(el){
+        if(el.id === el.servings){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+    return results;
+}
+
+let MatchingFood = problemThree();
+console.log('Matching Foods: ', MatchingFood)
+//4. Create a function that will return only dishes whose serving count is even.
+//Filter
+function problemFour(){
+    let results = dishes.filter(function(el){
+        if(el.servings % 2 === 0){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+    return results;
+}
+
+let EvenFood = problemFour();
+console.log('Even Foods: ', EvenFood)
+//5. Create a function that will return dishes whose ingredients array is "tomato" and "cheese".
+//Filter
+function problemFive(){
+    let results = dishes.filter(function(el){
+        if(el.ingredients[0] === "tomato" && el.ingredients[1] === "cheese"){
+            return true;
+        }
+        else{
+            return false;
+        }})
+
+    return results;
+}
+
+let TCFood = problemFive();
+console.log('Food with tomato and cheese: ', TCFood)
+//6a. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
+//Map
+function problemSixa(){
+    let results = dishes.map(function(el){
+        for (i in el.cuisine)
+        return el.cuisine
+        
+    })
+    return results
+
+}   
+let Cuisines = problemSixa();
+console.log('All Cuisines: ', Cuisines)
+// BONUS: (come back to this after finishing all)
+//6b. Use the filter method to eliminate duplicates, leaving only distinct values in the array
+
+//7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
+//Map 
+function problemSeven(){
+    let results = dishes.map(function(el){
+    
+        return el.cuisine + " " + el.name;
+        
+       
+    })
+    return results;
+
+}   
+let CusineandDish = problemSeven();
+console.log('All Cuisines and Dishes: ', CusineandDish);
+//8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
+//Map, Filter
+function problemEight(){
+    let vegetariandish = dishes.filter(function(el){
+        if(el.cuisine === "Vegetarian"){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+    let results = vegetariandish.map(function(el){
+        
+        return el.cuisine + " " + el.name;
+    
+   
+    })
+    return results;
+
+}
+let Vegetariandish = problemEight()
+console.log ('All Vegetarian Dishes: ', Vegetariandish)   
+//BONUS
+//9. Create a function that will return dishes whose ingredients array INCLUDES "chickpea".
+//Hint: You do not want to check the array's indexes to find out what the array INCLUDES.
+//Filter
+
+//10. Create a function that will return the total serving count of all dishes.
+//Must use Reduce, not a loop.
+
+//11. Create a function that will return an array of any objects that do not share a cuisine type with any other objects.
+
